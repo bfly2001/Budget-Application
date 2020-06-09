2b354bf71e38ebfc0d7e692db89ccea20eaaee54
bfly2001 <brendan.sflynn@gmail.com>
2020-06-09 13:41:34 -0700

    Update app.js
    Moved event listeners inside controller function. Added init function.

--- app.js
+++ app.js
@@ -35,7 +35,17 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+         ctrlAddItem();
+       }
+    });
+  };
 
   var ctrlAddItem = function() {
 
@@ -50,14 +60,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
-  }
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 || event.which === 13)
-       ctrlAddItem();
-
-  });
+  return {
+    init: function() {
+      console.log('Application has started.');
+      setupEventListeners();
+    }
+  };
 
 })(budgetController, UIController);
+
+controller.init();
